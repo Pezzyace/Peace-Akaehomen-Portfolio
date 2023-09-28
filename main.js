ec2a3e8b94ae4201ba281a59f32fc37812961823
Akaehomen peace <peacedavid049@gmail.com>
2023-09-28 02:27:29 +0100

    commit

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+const hamburger = document.getElementById('nav');
+const navigation = document.getElementById('navigation');
+const chevron = document.getElementById('close-btn');
+
+
 //change navbar styles on scroll
 
 window.addEventListener('scroll', () => {
   document.querySelector('.desktop-nav').classList.toggle('window-scroll', window.scrollY > 100);
+})
+
+
+// navigation btns
+
+hamburger.addEventListener('click', () => {
+  navigation.style.display = 'flex';
+})
+
+chevron.addEventListener('click', () => {
+  navigation.style.display = 'none';
 })
