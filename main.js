99f9ef989729af4012742c17cdaf7b05702b3dbe
Akaehomen peace <peacedavid049@gmail.com>
2024-01-08 18:22:39 +0100

    commit

--- main.js
+++ main.js
@@ -13,9 +13,27 @@ window.addEventListener('scroll', () => {
 // navigation btns
 
 hamburger.addEventListener('click', () => {
-  navigation.style.display = 'flex';
+  navigation.classList.add("show");
 })
 
 chevron.addEventListener('click', () => {
-  navigation.style.display = 'none';
+  navigation.classList.remove("show");
 })
+
+// Scroll into view sequence
+
+const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
+  });
